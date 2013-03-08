2e432f71bac5e50548ee75954e3117e6fe72717d
Johnny Domino <johnny@brandingbrand.com>
2013-03-07 22:35:46 -0500

    set app name

--- app.js
+++ app.js
@@ -3,14 +3,16 @@
  * Module dependencies.
  */
 
-var express  = require('express')
-  , clustrap = require('clustrap')
-  , zip  = require('./routes/zip')
-  , middleware = require('./lib/middleware');
+var express    = require('express')
+  , clustrap   = require('clustrap')
+  , zip        = require('./routes/zip')
+  , middleware = require('./lib/middleware')
+  , pkg        = require('./package.json');
 
 var app = express();
 
 app.configure(function(){
+  app.set('name', pkg.name);
   app.set('port', process.env.PORT || 3000);
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
