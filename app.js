91d6851aa7cb4137c9f7359324fa962b312e011b
Johnny Domino <johnny@brandingbrand.com>
2013-03-07 23:23:05 -0500

    remove logger in production

--- app.js
+++ app.js
@@ -11,16 +11,16 @@ var express    = require('express')
 
 var app = express();
 
+app.configure('development', function(){
+  app.use(express.logger('dev'));
+  app.use(express.errorHandler());
+});
+
 app.configure(function(){
   app.set('name', pkg.name);
   app.set('port', process.env.PORT || 3000);
-  app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(app.router);
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler());
 });
 
 app.get('/zips/:zip', middleware.makeCacheable, middleware.allowCrossDomain, zip.details);
